9c2d385fa42d01e048cf39a2c70388fa5529f9d2
Saul Rosales B <saul.rosalesb@gmail.com>
2023-10-15 19:22:32 -0400

    fix: removed extra spaces

--- src/styles.ts
+++ src/styles.ts
@@ -2,12 +2,12 @@ import cx from '@architecturex/utils.cx'
 
 export const styles = {
   switcherContainer: 'relative inline-block my-8 leading-9',
-  switcherLabel: 'relative inline-block w-16 h-8 ',
+  switcherLabel: 'relative inline-block w-16 h-8',
   switcherText: 'px-4 text-base text-gray-700',
   switcherInput: 'hidden',
   switcherSlider: 'absolute cursor-pointer top-0 left-0 right-0 bottom-0 rounded-3x1',
-  roundSwitchStyles: ' cursor-pointer block  h-4 rounded-full ',
-  squareSwitchStyles: ' cursor-pointer block  h-4 ',
+  roundSwitchStyles: 'cursor-pointer block h-4 rounded-full',
+  squareSwitchStyles: 'cursor-pointer block h-4',
   sliderStyle:
     'absolute cursor-pointer block w-4 h-4 bg-white rounded-full shadow transform transition ease-in',
   primary: 'bg-blue-500',
